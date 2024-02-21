893bb9a3a70a26465db486d5c6e3b10615a5314c
huangxi <huangxi@quyiyuan.com>
2024-02-21 18:03:08 +0800

    初始化项目

---
+++ gateway/src/main/java/com/qy/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.qy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+/**
+ * @author 17691127401
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+//        SpringApplication.run(GatewayApplication.class, args);
+        new ArrayList<>().stream().spliterator()
+    }
+
+}
